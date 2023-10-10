b3da6cae017f868d5d761a34f58dadfd3ed2af15
Cvieira <dullvieira@gmail.com>
2023-10-10 10:04:52 +0100

    [feature] ui-sidebar: added ui-sidebar component

--- libs/ui/ui-sidebar/src/lib/ui-sidebar.module.ts
+++ libs/ui/ui-sidebar/src/lib/ui-sidebar.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiSidebarComponent } from './ui-sidebar/ui-sidebar.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [UiSidebarComponent],
+  exports: [UiSidebarComponent],
 })
 export class UiSidebarModule {}
