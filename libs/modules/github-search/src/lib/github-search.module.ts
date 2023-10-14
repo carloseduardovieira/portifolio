10697096f336f25f8f35c55598d0706575098e10
Cvieira <dullvieira@gmail.com>
2023-10-14 15:24:15 +0100

    [feature] translate: added translate packages

--- libs/modules/github-search/src/lib/github-search.module.ts
+++ libs/modules/github-search/src/lib/github-search.module.ts
@@ -1,11 +1,38 @@
-import { SearchInputModule } from './../../../../shared/search-input/src/lib/search-input.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { GithubSearchComponent } from './github-search/github-search.component';
 import { GithubSearchRoutingModule } from './github-search.routing.module';
+import { SearchInputModule } from '@portifolio/search-input';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(
+    http,
+    'assets/i18n/translations/git/',
+    '.json'
+  );
+}
 
 @NgModule({
-  imports: [CommonModule, GithubSearchRoutingModule, SearchInputModule],
+  imports: [
+    CommonModule,
+    GithubSearchRoutingModule,
+    SearchInputModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   declarations: [GithubSearchComponent],
 })
 export class GithubSearchModule {}
--- libs/shared/search-input/src/lib/search-input.module.ts
+++ libs/shared/search-input/src/lib/search-input.module.ts
@@ -1,3 +1,4 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchInputComponent } from './search-input/search-input.component';
@@ -5,7 +6,7 @@ import { SearchInputComponent } from './search-input/search-input.component';
 const components = [SearchInputComponent];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   declarations: [...components],
   exports: components,
 })
