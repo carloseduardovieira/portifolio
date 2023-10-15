eea162cef92cbd64416402b8466c5dd5b34033fa
Cvieira <dullvieira@gmail.com>
2023-10-15 12:25:47 +0100

    [feature] list: added trackby into html

--- libs/shared/list/src/lib/list/interfaces/search-input.management.interface.ts
+++ libs/shared/list/src/lib/list/interfaces/search-input.management.interface.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 
 export interface SearchInputManagementInterface<M> {
   bindLabel: string;
+  bindValue?: string;
   termValidator?: RegExp;
   loadItemsByTerm(term: string): Observable<M[]>;
 }
