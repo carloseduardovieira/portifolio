21ae2b144a69f85a7d52ee71c1eb3c7bfc238ed0
Carlos Vieira <dullvieira@gmail.com>
2024-05-08 23:27:05 +0100

    [feature] select: added control management to select component

--- core/src/lib/components/dynamic-form-controls/ben-select/ben-select.component.ts
+++ core/src/lib/components/dynamic-form-controls/ben-select/ben-select.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CONTROL_DATA } from './../../../pipes/control-injector/control-data.token';
 
 @Component({
   selector: 'ben-select',
   standalone: true,
   imports: [CommonModule],
-  template: ``,
+  template: `
+    <select [id]="control.config.id" [value]="control.config.value">
+      @for(option of control.config.options; track control.config.id){
+        <option [value]="option.value">{{option.label}}</option>
+      }
+    </select>
+  `,
   styles: ``,
 })
-export class BenSelectComponent {}
+export class BenSelectComponent {
+  control = inject(CONTROL_DATA);
+}
