c5165b87a3a5685278aaf3e259684cfa7af68262
Carlos Vieira <dullvieira@gmail.com>
2024-06-30 10:26:34 +0100

    [feature] dynamic form: started using form control in ben-select

--- core/src/lib/components/dynamic-form-controls/ben-select/ben-select.component.ts
+++ core/src/lib/components/dynamic-form-controls/ben-select/ben-select.component.ts
@@ -1,20 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CONTROL_DATA } from './../../../pipes/control-injector/control-data.token';
+import { ControlContainer, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ben-select',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
+  <ng-container [formGroup]="formGroup">
     <select [id]="control.config.id" [value]="control.config.value">
       @for(option of control.config.options; track control.config.id){
         <option [value]="option.value">{{option.label}}</option>
       }
     </select>
+  </ng-container>
   `,
   styles: ``,
 })
 export class BenSelectComponent {
   control = inject(CONTROL_DATA);
+  
+  get formGroup(): FormGroup {
+    return this.parentFormGroup.control as FormGroup;
+  }
+
+  private parentFormGroup = inject(ControlContainer);
 }
