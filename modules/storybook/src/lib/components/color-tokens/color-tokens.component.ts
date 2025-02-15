5ceb0696e67e4f39c2e9ba5edc7326fb71f318b8
Carlos Vieira <dullvieira@gmail.com>
2025-02-15 22:51:19 +0000

    feat. start refactoring color tokens to be a table

--- modules/storybook/src/lib/components/color-tokens/color-tokens.component.ts
+++ modules/storybook/src/lib/components/color-tokens/color-tokens.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'cvieira-color-tokens',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './color-tokens.component.html',
   styleUrl: './color-tokens.component.scss',
 })
